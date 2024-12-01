263a62c708f91bdd1e7c804ae1551b0f33b708f7
ShadyRaccoon <marianbogdan953@yahoo.com>
2024-12-01 13:06:05 +0200

    bmake and item10 changes

--- item10.cpp
+++ item10.cpp
@@ -8,7 +8,7 @@ item10::item10(int width, int height, std::string color):
         << "OBIECT GENERAT:\n"
         << "INALTIME : " << height << " | "
         << "LATIME : " << width << " | "
-        << "CULOARE :" << color << " | ";
+        << "CULOARE :" << color << std::endl;
 };
 
 
@@ -22,21 +22,29 @@ item10::item10(const item10&& other):
         std::cout << "MOVE CONSTRUCTOR APELAT!\n"; 
 };
 
+item10::~item10(){
+    std::cout << "DESTRUCTOR APELAT!\n";
+}
+
 item10& item10::operator*=(const item10& opdr){
     std::cout << "ASSIGNMENT OPERATOR *= APELAT!\n"; 
+    this->height = 44;
     return *this;
 }
 
 item10& item10::operator+=(const item10& opdr){
     std::cout << "ASSIGNMENT OPERATOR += APELAT!\n"; 
+    this->height = 22;
     return *this;
 }
 item10& item10::operator-=(const item10& opdr){
     std::cout << "ASSIGNMENT OPERATOR -= APELAT!\n"; 
+    this->height = 33;
     return *this;
 }
 
 item10& item10::operator=(const item10& opdr){
     std::cout << "ASSIGNMENT OPERATOR = APELAT!\n"; 
+    this->height = 11;
     return *this;
 }
