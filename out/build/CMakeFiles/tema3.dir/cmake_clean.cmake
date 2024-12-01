edc169a0804fec569f1f41def90249d2b7a8637b
ShadyRaccoon <marianbogdan953@yahoo.com>
2024-12-01 13:04:03 +0200

    solve build issues

---
+++ out/build/CMakeFiles/tema3.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tema3.dir/item10.cpp.o"
+  "CMakeFiles/tema3.dir/item10.cpp.o.d"
+  "CMakeFiles/tema3.dir/item11.cpp.o"
+  "CMakeFiles/tema3.dir/item11.cpp.o.d"
+  "CMakeFiles/tema3.dir/item12.cpp.o"
+  "CMakeFiles/tema3.dir/item12.cpp.o.d"
+  "CMakeFiles/tema3.dir/main.cpp.o"
+  "CMakeFiles/tema3.dir/main.cpp.o.d"
+  "tema3"
+  "tema3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tema3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ out/build/CMakeFiles/tema3.dir/item12.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/tema3.dir/item12.cpp.o: \
+ /home/bogdan/Desktop/tema3paoo/item12.cpp /usr/include/stdc-predef.h
